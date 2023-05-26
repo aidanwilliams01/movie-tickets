82fa7bb0c00e1c8852f0b03f6bc349317aa3d1a6
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-26 16:29:04 -0700

    add HTML and functionality that displays ticket price based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,29 @@ function Ticket(firstRelease, matinee, senior) {
   this.firstRelease = firstRelease;
   this.matinee = matinee;
   this.senior = senior;
-  if (firstRelease === true) {
+  if (firstRelease === "true") {
     this.price = 20;
   }
   else {
     this.price = 15;
   }
-  if (matinee === true) {
+  if (matinee === "true") {
     this.price -= 5;
   }
-  if (senior === true) {
+  if (senior === "true") {
     this.price -= 5;
   }
 }
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const firstRelease = document.querySelector("input[name='movie']:checked").value;
+  const matinee = document.querySelector("input[name='time']:checked").value;
+  const senior = document.querySelector("input[name='senior']:checked").value;
+  const newTicket = new Ticket(firstRelease, matinee, senior);
+  document.querySelector("p").innerText = `Price: $${newTicket.price}`;
+}
+
+window.addEventListener("load", function () {
+  this.document.querySelector("form").addEventListener("submit", handleFormSubmission);
+});
